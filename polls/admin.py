5a6dc89c25fe219c277c1b50ad9612c265db8993
matsushii <mtsmath24@yahoo.co.jp>
2023-01-03 23:30:02 +0900

    管理ページにてChoiceを変更できるようadmin.pyを編集
    管理ページでChoiceを変更できるようmodelのインポート、クラスの作成を行いました。
    ・TabularInlineはStackedInlineよりもスマート

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+    
 class QuestionAdmin(admin.ModelAdmin):
-    fields = [ (None,{'fields': ['question_text']}),
-             ('Date information', {'fields':['pub_date']})]
-
+    fieldsets = [ (None,{'fields': ['question_text']}),
+             ('Date information', {'fields':['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    
 admin.site.register(Question, QuestionAdmin)
